31e21d856ab7228ac40e6f39e267dbf105d41feb
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-01-15 22:06:54 -0500

    created simple script to dump google sheet into json file. WIP

--- seeds/googleSheetFetch.js
+++ seeds/googleSheetFetch.js
@@ -1,20 +1,57 @@
+import { google } from "googleapis";
+import "dotenv/config";
+import * as fs from "fs";
+
+const sheets = google.sheets({
+	version: "v4",
+	auth: process.env.API_KEY,
+});
+
 let historicalSheet2022Id = "1IKULDleiK1QEHUt0y1pF7zE3KB3JOMfoDzKpm7x6tuo";
 let historicalSheet2023Id = "1phCjeMld4QMeYgafXvLoLA4kWMmfzt3Mk6jzasWb1hQ";
 
 class GoogleSpreadsheet {
-	#baseURL = "https://docs.google.com/spreadsheets/d/";
-	constructor(id) {
+	constructor(id, properties, sheets, url) {
 		this.id = id;
-		this.baseSheet = newSheet(0);
-		this.sheets = [baseSheet];
-		this.url = new URL(`${this.id}/edit#gid=0`, this.#baseURL); //edit#gid=0 is the base Sheet
+		this.properties = properties;
+		this.sheets = sheets;
+		this.URL = new URL(url);
 	}
 }
 
 class Sheet {
-	#baseURL = "/edit";
-	constructor(id) {
+	constructor(id, title, index, data, merges = []) {
 		this.id = id;
-		this.URL = new URL(`#gid=${this.id}`, this.#baseURL);
+		this.title = title;
+		this.index = index;
+		this.data = data;
+		this.merges = merges;
 	}
 }
+
+// const params = { spreadsheetId: historicalSheet2023Id };
+
+async function main() {
+	const res = await sheets.spreadsheets.get({
+		spreadsheetId: historicalSheet2023Id,
+		includeGridData: true,
+	});
+
+	const data = JSON.stringify(res.data.sheets[1]);
+
+	fs.writeFile("data.json", data, (error) => {
+		if (error) {
+			console.error(error);
+			throw error;
+		}
+
+		console.log("JSON data written to data.json");
+	});
+
+	//console.log(res.data.sheets[1].data[0].rowData);
+	//res.data returns the entire spreadsheet
+	//res.data.sheets returns the list of sheets within the spreadsheet
+	//res.data.sheets[1] returns the 2nd sheet within the sheets array
+	//res.data.sheets[1].data returns the cell data for that specific sheet
+}
+main().catch(console.error);
