b3480bb3430f09ba94e2b4daf824cb0f30b1267d
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-01-22 21:04:50 -0500

    revised class scheme to week, data, and games

--- seeds/googleSheetFetch.js
+++ seeds/googleSheetFetch.js
@@ -2,6 +2,41 @@ import { google } from "googleapis";
 import "dotenv/config";
 import * as fs from "fs";
 
+class Week {
+	constructor(number, matchUps, data) {
+		this.number = number;
+		this.matchUps = matchUps;
+		this.data = data;
+	}
+}
+
+class Data {
+	constructor(name, games) {
+		this.name = name;
+		this.games = games;
+	}
+}
+
+class Game {
+	constructor(
+		number,
+		homeTeam,
+		awayTeam,
+		pickedTeam,
+		winningTeam,
+		pointsWagered,
+		pointsWon
+	) {
+		this.number = number;
+		this.homeTeam = homeTeam;
+		this.awayTeam = awayTeam;
+		this.pickedTeam = pickedTeam;
+		this.winningTeam = winningTeam;
+		this.pointsWagered = pointsWagered;
+		this.pointsWon = pointsWon;
+	}
+}
+
 const sheets = google.sheets({
 	version: "v4",
 	auth: process.env.API_KEY,
@@ -10,32 +45,43 @@ const sheets = google.sheets({
 let historicalSheet2022Id = "1IKULDleiK1QEHUt0y1pF7zE3KB3JOMfoDzKpm7x6tuo";
 let historicalSheet2023Id = "1phCjeMld4QMeYgafXvLoLA4kWMmfzt3Mk6jzasWb1hQ";
 
-class GoogleSpreadsheet {
-	constructor(id, properties, sheets, url) {
-		this.id = id;
-		this.properties = properties;
-		this.sheets = sheets;
-		this.URL = new URL(url);
-	}
-}
-
-class Sheet {
-	constructor(id, title, index, data, merges = []) {
-		this.id = id;
-		this.title = title;
-		this.index = index;
-		this.data = data;
-		this.merges = merges;
-	}
-}
-
-// const params = { spreadsheetId: historicalSheet2023Id };
-
 async function main() {
 	const res = await sheets.spreadsheets.get({
 		spreadsheetId: historicalSheet2023Id,
 		includeGridData: true,
 	});
+
+	let players = 11;
+	let sheets = res.data.sheets;
+	let matchups = [];
+	let weekData = [];
+
+	for (let i = 1; i < sheets.length - 3; i++) {
+		// Generate Matchup List
+		for (
+			let j = 2;
+			j < sheets[i].data[0].rowData[5].values.length;
+			j += 2
+		) {
+			if (
+				Object.hasOwn(
+					sheets[i].data[0].rowData[5].values[j],
+					"formattedValue"
+				)
+			) {
+				matchups.push(
+					sheets[i].data[0].rowData[5].values[j].formattedValue
+				);
+			}
+		}
+
+		// Generate Player Pick Data
+		for (let k = 0; k < players; k++) {
+			sheets[i].data[0].rowData;
+		}
+
+		weekData.push(new Week(i, matchups, pickData));
+	}
 
 	const data = JSON.stringify(res.data.sheets[1]);
 
