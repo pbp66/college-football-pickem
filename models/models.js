a1b20800ca966857429ce947ece0343ee100225d
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-02-01 19:40:44 -0500

    fixed init bug by re-ordering model.js import statements

--- models/models.js
+++ models/models.js
@@ -1,11 +1,43 @@
-import Games from "./games.js";
-import Locations from "./locations.js";
-import Picks from "./picks.js";
+import Weeks from "./weeks.js";
+import Users from "./users.js";
 import Teams from "./teams.js";
 import Usernames from "./usernames.js";
-import Users from "./users.js";
-import Weeks from "./weeks.js";
+import Locations from "./locations.js";
+import Games from "./games.js";
+import Picks from "./picks.js";
 
+Weeks.hasMany(Games, {
+	foreignKey: "week",
+});
+Weeks.hasMany(Picks, {
+	foreignKey: "week",
+});
+Users.hasMany(Usernames, {
+	foreignKey: "user",
+	// Or:
+	// foreignKey: { name: "user"},
+});
+Users.hasMany(Picks, {
+	foreignKey: "user",
+});
+Teams.hasMany(Picks, {
+	foreignKey: "picked_team",
+});
+Teams.hasMany(Games, {
+	foreignKey: "home_team",
+});
+Teams.hasMany(Games, {
+	foreignKey: "away_team",
+});
+Teams.hasMany(Games, {
+	foreignKey: "winning_team",
+});
+Locations.hasMany(Teams, {
+	foreignKey: "location",
+});
+Locations.hasMany(Games, {
+	foreignKey: "location",
+});
 Games.belongsTo(Teams, {
 	as: "home_team",
 	foreignKey: "id",
@@ -23,49 +55,11 @@ Games.belongsTo(Locations);
 Games.hasMany(Picks, {
 	foreignKey: "game",
 });
-
-Locations.hasMany(Teams, {
-	foreignKey: "location",
-});
-Locations.hasMany(Games, {
-	foreignKey: "location",
-});
-
 Picks.belongsTo(Users);
 Picks.belongsTo(Teams);
 Picks.belongsTo(Games);
 Picks.belongsTo(Weeks);
-
 Teams.belongsTo(Locations);
-Teams.hasMany(Picks, {
-	foreignKey: "picked_team",
-});
-Teams.hasMany(Games, {
-	foreignKey: "home_team",
-});
-Teams.hasMany(Games, {
-	foreignKey: "away_team",
-});
-Teams.hasMany(Games, {
-	foreignKey: "winning_team",
-});
-
 Usernames.belongsTo(Users);
-
-Users.hasMany(Usernames, {
-	foreignKey: "user",
-	// Or:
-	// foreignKey: { name: "user"},
-});
-Users.hasMany(Picks, {
-	foreignKey: "user",
-});
-
-Weeks.hasMany(Games, {
-	foreignKey: "week",
-});
-Weeks.hasMany(Picks, {
-	foreignKey: "week",
-});
 
 export { Games, Locations, Picks, Teams, Usernames, Users, Weeks };
